db1e2f76143bcfcb4859265414c62d2aeec44273
Shawn <214118699@qq.com>
2016-01-21 21:53:37 +0800

    add tags model

---
+++ models/tags_model.js
@@ -0,0 +1,48 @@
+var mongoose = require('../lib/mongoose'),
+	Schema = mongoose.Schema,
+	COLLECTION_NAME = 'tags',
+	autoIncrement = require('mongoose-auto-increment'),
+	_ = require('lodash');
+
+var tagSchema = new Schema({
+	//唯一自增ID
+	tid: {
+		type     : Number,
+		required : true
+	},
+	name : {
+		type     : String,
+		required : true
+	},
+	st : {
+		type     : Number,
+		required : true,
+		default  : 0
+	},
+	cts : {
+		type       : Date,
+		required   : true,
+		default    : Date.now
+	},
+	pts : {
+		type       : Date,
+		required   : true,
+		default    : Date.now
+	}
+});
+
+userSchema.index({tid: 1});
+userSchema.index({cts: 1});
+
+userSchema.plugin(autoIncrement.plugin, {
+	model   : 'tags',
+	field   : 'tid',
+	startAt : 1
+});
+
+_.extend(
+	module.exports,
+	{
+		Model: mongoose.model(COLLECTION_NAME, tagSchema)
+	}
+);
