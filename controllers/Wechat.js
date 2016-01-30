64f57f07c34a96a4df7ec27d818d53fcc6632283
Shawn <214118699@qq.com>
2016-01-30 11:59:21 +0800

    auth config

--- controllers/Wechat.js
+++ controllers/Wechat.js
@@ -1,7 +1,14 @@
 var _ = require('lodash'),
+	crypto = require("crypto"),
 	config =  require('./../config');
 
 
+function sha1(str){
+	var md5sum = crypto.createHash("sha1");
+	md5sum.update(str);
+	str = md5sum.digest("hex");
+	return str;
+}
 /**
  * 微信认证
  * @param req
@@ -9,9 +16,27 @@ var _ = require('lodash'),
  */
 function wechatAuth(req, res){
 	var query = req.query,
-		echostr = query.echostr || '';
+		signature = query.signature || '',
+		echostr = query.echostr || '',
+		timestamp = query['timestamp'] || '',
+		nonce = query.nonce || '',
+		oriArray = new Array();
 
-	return res.json({echostr : echostr});
+	oriArray[0] = nonce,
+	oriArray[1] = timestamp;
+	oriArray[2] = "e574f89de1fb4d977c3bddcfce3ab640";
+	oriArray.sort();
+	var original = oriArray.join('');
+
+	var scyptoString = sha1(original);
+
+	if(signature == scyptoString){
+		res.end(echostr);
+		console.log("Confirm and send echo back");
+	}else {
+		res.end("false");
+		console.log("Failed!");
+	}
 }
 
 _.extend(
