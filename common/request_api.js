d1d30110d1c7b73ce3f91f13ec6bfdb20ae84206
Shawn <214118699@qq.com>
2016-02-03 14:57:41 +0800

    ebooks for wechat

---
+++ common/request_api.js
@@ -0,0 +1,66 @@
+/**
+ * This is a wrapper of request.
+ * Reference: https://github.com/mikeal/request
+ */
+
+var _ = require('lodash'),
+	request = require('request'),
+	config = require('./../config');
+
+var request_extend = request.defaults({timeout: config.api_timeout});
+
+
+/**
+ * 发送get请求
+ * @param url 是完整的url，例如：'http://it-ebooks-api.info/v1/'
+ * @param data 例如： {st: 100}
+ * @param cb
+ */
+function request_get(url, data, cb){
+	if(_.isFunction(data)){
+		cb = data;
+		data = {};
+	}
+
+	var request_options = {
+		uri: url,
+		method: 'GET',
+		qs: data,
+		json: true
+	};
+
+	request_extend(request_options, function(err, response, body) {
+		return cb(err, body);
+	});
+}
+
+/**
+ * 发送post请求
+ * @param url 是完整的url，例如：'http://it-ebooks-api.info/v1/search/php'
+ * @param data 例如： {id: 100}
+ * @param cb
+ */
+function request_post(url, data, cb){
+	if(_.isFunction(data)){
+		cb = data;
+		data = {};
+	}
+
+	var request_options = {
+		uri: url,
+		method: 'POST',
+		body: data,
+		json: true
+	};
+
+	request_extend(request_options, function(err, response, body) {
+		return cb(err, body);
+	});
+}
+
+_.extend(module.exports, {
+	request_extend: request_extend,
+	request_get: request_get,
+	request_post: request_post
+});
+
--- controllers/Wechat.js
+++ controllers/Wechat.js
@@ -1,18 +1,38 @@
 var _ = require('lodash'),
 	config =  require('./../config'),
-	wechat = require('wechat');
+	wechat = require('wechat'),
+	ebooks = require('./../lib/ebooks');
 
 exports.wechatAPI = wechat(config.wechat.Token, function(req, res, next){
-	var message = req.weixin;
-
-	if (message.Content === '呵呵') {
-
-		res.reply('呵呵你妹');
-	}
+	var message = req.weixin,
+		inputMsg = message.Content;
 
 
 	if((message.MsgType == 'event') && (message.Event == 'subscribe')){
-		res.reply('关注个毛啊');
+		res.reply('感谢关注');
+	}
+
+	if(inputMsg){
+		ebooks.search(inputMsg, function(err, result){
+			if(err) {
+				res.reply('您的请求上天了');
+			}
+
+			var books= result.Books || [];
+
+			var replyBooks = _.map(books, function(book){
+				var replyBook = {};
+
+				replyBook.title = book.Title;
+				replyBook.description = book.Description;
+				replyBook.picurl = book.Image;
+				replyBook.url = 'http://www.baidu.com/';
+
+				return replyBook;
+			});
+
+			res.reply(replyBooks);
+		});
 	}
 
 	return next();
