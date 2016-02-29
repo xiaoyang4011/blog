b5270c81bd06f85492575eec9308006a7f95fab1
Shawn <214118699@qq.com>
2016-02-29 21:28:47 +0800

    tag fix csrf bug

--- routes/blog.js
+++ routes/blog.js
@@ -13,7 +13,7 @@ router.get('/about-me-edit', auth.userRequired, Blog.about_edit);
 router.get('/about-me', Blog.about_me);
 router.get('/tags', auth.userRequired, Blog.tags);
 router.get('/add_tag', auth.userRequired, Blog.add_tag);
-router.post('/save_tag', auth.userRequired, Blog.save_tag);
+router.post('/do_save_tag', auth.userRequired, Blog.save_tag);
 router.get('/edit_tag', auth.userRequired, Blog.edit_tag);
 router.get('/upload', auth.userRequired, Blog.uploadFile);
 router.get('/uptoken', auth.userRequired, Blog.upToken);
